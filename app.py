ce654fbfef9ad54b8ce6e69567baa2b19363e26c
VinuthaST-vincy <vinuthastvincy@gmail.com>
2025-03-27 06:10:30 +0000

    Add py file

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
     return "Hello_World"
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=3000, debug=True)
 
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
